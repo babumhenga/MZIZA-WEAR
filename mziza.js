2aaf381d6539b67f30f3e3709a113e5c6a6266bb
babumhenga <njogualex60@gmail.com>
2024-04-11 22:32:44 +0300

    Update CSS files with linear gradient backgrounds and add social media links to footer
    The following changes were made:
    
    1. `cart.css`: Update navbar background color with linear gradient
    2. `gwanda.css`: Update navbar background color with linear gradient
    3. `index.html`:
    	* Add social media links to footer
    	* Update footer layout
    4. `jezi.css`:
    	* Update navbar background color with linear gradient
    	* Add margin to image in navbar
    5. `mziza.css`: Update navbar background color with linear gradient

---
+++ mziza.js
@@ -0,0 +1,36 @@
+// Get the cart button and cart overlay
+const cartBtn = document.querySelector(".cart-btn");
+const cartOverlay = document.querySelector(".cart-overlay");
+const cartItems = document.querySelector(".cart-items");
+const cartTotal = document.querySelector(".cart-total");
+const cartContent = document.querySelector(".cart-content");
+
+// Get the products from diffrent html
+class Products {
+  async getProducts() {
+    try {
+      let result = await fetch("products");
+      return await result.json();
+    } catch (error) {
+      console.log(error);
+    }
+  }
+}
+
+
+  // Remove the item from the cart
+  removeItem(item) {
+    const removeItem = item.querySelector(".remove-item");
+    removeItem.addEventListener("click", e => {
+      const id = e.target.dataset.id;
+      cart = cart.filter(item => item.id!== parseInt(id));
+      Storage.saveCart(cart);
+      this.setCartValues(cart);
+      item.remove();
+      this.removeItemFromCartOverlay(id);
+    });
+  }
+.classList.remove("close-cart");
+
+}
+
